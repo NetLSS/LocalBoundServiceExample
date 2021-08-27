373eee859a0f0194c3df1d6c65da0d0f5ccf16a7
NetLSS <futureprogrammer@naver.com>
2021-08-27 14:40:06 +0900

    Feat: 서비스에 클라이언트 바인딩 ServiceConnection 등.

--- ReadMe.md
+++ ReadMe.md
@@ -50,3 +50,37 @@ class BoundService : Service() {
     }
 }
 ```
+
+## Binder 구현
+
+```kotlin
+package com.lilcode.example.localboundservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import java.text.SimpleDateFormat
+import java.util.*
+
+class BoundService : Service() {
+
+    private val myBinder: IBinder = MyLocalBinder() // Binder 는 IBinder 의 서브 클래스
+
+    override fun onBind(intent: Intent): IBinder {
+        return myBinder
+    }
+
+    // 현재 시간 반환. 클라이언트에서 호출할 함수임.
+    fun getCurrentTime(): String {
+        val dateFormat = SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US)
+        return dateFormat.format(Date())
+    }
+
+    inner class MyLocalBinder : Binder() { // inner 클래스로 바인더의 서브 클래스 생성
+        fun getService() : BoundService { // 바운드 서비스의 인스턴스를 반환 (클라이언트에서 사용)
+            return this@BoundService // 바운드 서비스의 현재 인스턴스 참조를 반환한다.
+        }
+    }
+}
+```
--- app/src/main/java/com/lilcode/example/localboundservice/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/localboundservice/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.lilcode.example.localboundservice
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.lilcode.example.localboundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = requireNotNull(_binding)
+
+    var myService: BoundService? = null
+    var isBound = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val intent = Intent(this, BoundService::class.java)
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE) // 서비스에 바인딩
+    }
+
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as BoundService.MyLocalBinder
+            myService = binder.getService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+        }
+
     }
 }
