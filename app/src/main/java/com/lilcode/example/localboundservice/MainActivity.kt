c2dbc7ae067c735092b1c43b889c9fe3ca30fbb8
NetLSS <futureprogrammer@naver.com>
2021-08-27 14:17:42 +0900

    Feat: viewBinding 셋팅

--- app/src/main/java/com/lilcode/example/localboundservice/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/localboundservice/MainActivity.kt
@@ -2,10 +2,14 @@ package com.lilcode.example.localboundservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lilcode.example.localboundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
