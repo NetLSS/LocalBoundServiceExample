b6ffe00bae9ad62ff3328f27e7523e91c17ea126
NetLSS <futureprogrammer@naver.com>
2021-08-27 14:31:26 +0900

    Feat: Binder 구현 #2

--- app/src/main/java/com/lilcode/example/localboundservice/BoundService.kt
+++ app/src/main/java/com/lilcode/example/localboundservice/BoundService.kt
@@ -4,13 +4,21 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import java.text.SimpleDateFormat
+import java.util.*
 
 class BoundService : Service() {
 
     private val myBinder: IBinder = MyLocalBinder() // Binder 는 IBinder 의 서브 클래스
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return myBinder
+    }
+
+    // 현재 시간 반환. 클라이언트에서 호출할 함수임.
+    fun getCurrentTime(): String {
+        val dateFormat = SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US)
+        return dateFormat.format(Date())
     }
 
     inner class MyLocalBinder : Binder() { // inner 클래스로 바인더의 서브 클래스 생성
